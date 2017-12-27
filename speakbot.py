3b5c8da915d509fd66bb318acb2278d3fe1fdb2d
dboyi <tdkingston911@gmail.com>
2017-12-26 21:41:01 -0800

    Add files via upload

---
+++ speakbot.py
@@ -0,0 +1,23 @@
+import time
+import random
+import datetime
+import telepot
+
+"""
+- `/roll` - reply with a random integer between 1 and 6, like rolling a dice.
+- `/time` - reply with the current time, like a clock.
+"""
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print 'Got command: %s' % command
+
+    if command == '/roll':
+        bot.sendMessage(chat_id, random.randint(1,6))
+    elif command == '/time':
+        bot.sendMessage(chat_id, str(datetime.datetime.now()))
+
+
+
